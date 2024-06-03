3ea6847a301503d34a4a7a1a96be619258810c17
Brando Miranda <brandojazz@gmail.com>
2024-06-03 16:42:44 -0700

    Lean 4 Project is a folder where the lean folder, lean 4 code, and install lean-toolchain, and lakefile.lean will be at

--- README.md
+++ README.md
@@ -10,20 +10,25 @@ Here is a link of live proving tutorials from this repo: https://www.youtube.com
 There is no guarantee it will work on your machine. 
 Therefore, understand each command and only run the next command if the current one succeeded. 
 
+## Main Concepts
+
+- Lean 4 Project := Usually a Lean 4 project is the root of the github repo. It usually has the `lakefile.lean`, `lean-toolchain` files, `.lake`, `Folder_With_Lean_Code`, etc. 
+In our case the root of the repo is not the Lean 4 project and instead with have a seperate folder for the Lean 4 source code (that will be the root of the Lean 4 project) at `learning_lean/lean_src_proj`. 
+- Github project root := For pure Lean 4 Projects it's the same as the root of the Lean 4 project root. For that's not the case and there might be an additional `py_src` too. 
+
 ## For developing Lean in this repo
 
 If you git cloned this repo with say `git clone git@github.com:brando90/learning_lean.git` then you will have the lean project `lean_src_proj` folder and it won't have it's Lean depedencies e.g., Mathlib or the `.lake` folder will be missing. 
 
 If that is the case, then you need to install mathlib for this project (note doing `lake +leanprover/lean4:nightly-2023-02-04 new my_project math` as suggested by the Lean [projects setup will fail](https://leanprover-community.github.io/install/project.html#creating-a-lean-project) does **not work**) with:
 ```bash
-# go to root main repo
+# Go to root main github repo (in this case, it's not the Lean 4 project)
 cd $HOME/learning_lean
 git clone git@github.com:brando90/learning_lean.git
-# go to lean proj
-mkdir lean_src_proj
+# Go to the Lean 4 proj path (at `learning_lean/lean_src_proj`)
 cd lean_src_proj
-# set up lean4 & mathlib
-##--> doesn't work here + it creates a .git folder that we don't want: lake +leanprover/lean4:nightly-2023-02-04 new my_project math (from Lean community)
+# Set up Lean 4 & Mathlib
+## Note: the official Lean 4 tutorial command `lake +leanprover/lean4:nightly-2023-02-04 new my_project math` doesn't work here + it creates a .git folder that we don't want
 lake update
 lake exe cache get
 code .
--- install.sh
+++ install.sh
@@ -1,22 +1,35 @@
-# -- Setup our lean_src_proj project for the first time (ref: https://leanprover-community.github.io/install/project.html#lean-projects)
-# - Go to a folder where you want to create a project in a subfolder lean_src, and type lake +leanprover/lean4:nightly-2023-02-04 new lean_src math
-# go to root of project (not the lean project)
+# -- Setup our lean_src_proj Lean 4 project for the first time (ref: https://leanprover-community.github.io/install/project.html#lean-projects)
+# - Go to a folder where you want to create a Lean 4 project (e.g., the root of this `learning_lean` git repo)
 cd $HOME/learning_lean
-# Option1: (from scratch) set up the main lean_src_proj project for the first time:
+# - Then create the Lean 4 project with the following options (note: usually a Lean 4 project's path **is** the same as the root of the github repo, but not for us)
+# Option 1: setting up the Lean 4 project at `lean_src_proj`` for the first time (& installing Mathlib)
 lake +leanprover/lean4:nightly-2023-02-04 new lean_src_proj math
-# Option 2: (for cloning project) set up main lean_src_proj project if you did a git clone of root learning_lean (so depedencies, e.g., matblib need to be installed)
-# mkdir lean_src_proj
+# Option 2: set up Lean 4 project at `lean_src_proj` if the path `lean_src_proj` already exists but Mathlib has not been installed (e.g., after git cloning the root git repo `learning_lean`)
 cd lean_src_proj
 lake update
 lake exe cache get
 # if you get an error message saying lake is an unknown command and you have not logged in since you installed Lean, then you may need to first type 
 # source ~/.profile or source ~/.bash_profile
-# -- Go inside the clean_src folder and type `lake update``, then `lake exe cache get`` and then `mkdir MyProject``
+# -- Go inside Lean 4 project dir and set it up 
+# cd to the Lean 4 project folder
 cd lean_src_proj
-# optionally: rm -rf lean_src_proj/.git
+# Update the dependencies and fetch the latest versions specified in the lakefile (in your Lean 4 project's at `lean_src_proj`)
 lake update 
+# Retrieve the cached files to speed up subsequent builds and ensure the latest cached versions are used
 lake exe cache get
-# -- Your Lean code should now be put inside files with extension .lean living in lean_src/MyProject/ or a subfolder thereof.
-mkdir MyProject
-cd MyProject
+# -- The Lean code *.lean for your Lean 4 project (located at `learning_lean/lean_src_proj`) should be at learning_lean/lean_src/MyProject/ or a subfolder thereof.
+# - Go to root of Lean 4 project in this github repo
+cd $HOME/learning_lean/learn_src_proj
+# Create 1st folder with the Lean 4 src code will like for our Lean 4 project (at the path `learning_lean/learn_src_proj`)
+mkdir MyLean4CodeFolder1
+cd MyLean4CodeFolder1
 echo 'import Mathlib.Topology.Basic\n\n#check TopologicalSpace' > Test.lean
+# Create 2nd folder with the Lean 4 src code will like for our Lean 4 project (at the path `learning_lean/learn_src_proj`)
+mkdir MyLean4CodeFolder2
+cd MyLean4CodeFolder2
+echo 'import Mathlib.Topology.Basic\n\n#check TopologicalSpace' > Test.lean
+# ...etc...
+# Note: confusingly, the official tutorial for creating a Lean 4 project (which for them is usually the root of the git repo) has a folder named `my_project/MyProject/` 
+# (or a subfolder thereof), for details see: https://leanprover-community.github.io/install/project.html
+# but for us our Lean 4 project is not the root but instead is at `learning_lean/lean_src_proj` with folder with *.lean files in the subfolders there e.g., 
+# `MyLean4CodeFolder1`, `MyLean4CodeFolder2`, ...etc... 
